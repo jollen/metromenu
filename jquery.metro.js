e56a7bf9d49406a0c2ac8db4a7f2b6af7a4799d2
Jollen Chen <jollen@moko365.com>
2012-11-09 18:05:40 +0800

    Add: Can use remote menu items.
    See samples/remote-api.html for more details. Now, we can call open API
    to get JSON data of menu items.

--- jquery.metro.js
+++ jquery.metro.js
@@ -2,7 +2,7 @@
  Metro Menu - Metro UI Style Menus - jQuery Plugin
 
  https://github.com/jollen/metromenu
- version 0.9pre1.121106
+ version 0.9pre2.121108
  Copyright (c) 2012 Jollen Chen
  Licensed under the MIT license
 */
@@ -11,7 +11,7 @@ $.fn.addMetroSimpleText = function (items) {
 	var html_code = "";
 	var ctx = $(this);
 	
-	ctx.empty().css("display", "none");
+	ctx.empty().css("display", "none");	
 	
 	for (var i = 0; i < items.length; i++) {
 		var bgcolor = items[i].bgcolor,
@@ -37,17 +37,39 @@ $.fn.AddMetroSimpleButton = function (theme, image, text, items, subCtx, id) {
 	var ctx = $(this);
 		
 	html_code += " id='" + id + "'";
-		
-	if (items != '') {
-	    html_code += " onclick='$(\"#" + subCtx + "\").addMetroSimpleText(" + JSON.stringify(items) + ");'";
-	}
-	html_code += " class='metro metrosingle " + theme + "'>\n";
-	html_code += "<div class='imgsimple'><img src='" + image + "' alt='" + text + "' /></div>\n";
-	html_code += "<span>" + text + "</span>\n";
-	html_code += "</div>\n";
 	
-	ctx.append(html_code);
-	ctx.fadeIn('slow');
+	// Determinte a JSON object or URL
+	if (typeof items === 'string') {
+	   			$.ajax({
+	   					url: items,
+	     				type: "POST",
+	                	dataType: "json",
+	                	complete: function(jqXHR, textStatus) {	  
+	                		// We need to parse and stringify again in 
+	                		// order to make Sarafi 6 working...
+							var items = JSON.parse(jqXHR.responseText);
+						    html_code += " onclick='$(\"#" + subCtx + "\").addMetroSimpleText(" + JSON.stringify(items) + ");'";  
+		
+							html_code += " class='metro metrosingle " + theme + "'>\n";
+							html_code += "<div class='imgsimple'><img src='" + image + "' alt='" + text + "' /></div>\n";
+							html_code += "<span>" + text + "</span>\n";
+							html_code += "</div>\n";
+							
+							ctx.append(html_code);
+							ctx.fadeIn('slow');         				
+						}
+	           		}); 
+	} else if (items != '') {
+	    html_code += " onclick='$(\"#" + subCtx + "\").addMetroSimpleText(" + JSON.stringify(items) + ");'";
+		
+		html_code += " class='metro metrosingle " + theme + "'>\n";
+		html_code += "<div class='imgsimple'><img src='" + image + "' alt='" + text + "' /></div>\n";
+		html_code += "<span>" + text + "</span>\n";
+		html_code += "</div>\n";
+		
+		ctx.append(html_code);
+		ctx.fadeIn('slow');
+	}
 };
 
 $.fn.AddMetroSimpleTile = function (theme, image, text, link, id) {
